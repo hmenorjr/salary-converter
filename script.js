161f76f32574ae01bfbefe19b9ec25bf257be20d
Herman Menor <herman.menor@gmail.com>
2023-08-28 12:46:32 +0000

    To monthly, yearly, quarterly and bi-monthly

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-document.getElementById("convertButton").addEventListener("click", function () {
-  const monthlySalary = parseFloat(document.getElementById("initialSalary").value);
-  const workHoursPerMonth = 160; // Assuming 40-hour workweek and 4 weeks in a month
+const monthlySalaryInput = document.getElementById("monthlySalary");
+const yearlySalaryInput = document.getElementById("yearlySalary");
+const quarterlySalaryInput = document.getElementById("quarterlySalary");
+const biMonthlySalaryInput = document.getElementById("biMonthlySalary");
 
-  const hourlyRate = monthlySalary / workHoursPerMonth;
+const monthlyOutput = document.getElementById("monthlyOutput");
+const yearlyOutput = document.getElementById("yearlyOutput");
+const quarterlyOutput = document.getElementById("quarterlyOutput");
+const biMonthlyOutput = document.getElementById("biMonthlyOutput");
 
-  const resultElement = document.getElementById("result");
-  resultElement.textContent = `Hourly Rate: $${hourlyRate.toFixed(2)}`;
+function calculateHourlyRate(salary, hoursPerPeriod) {
+  return salary / hoursPerPeriod;
+}
+
+monthlySalaryInput.addEventListener("input", function () {
+  const monthlySalary = parseFloat(monthlySalaryInput.value);
+  const hourlyRate = calculateHourlyRate(monthlySalary, 160);
+  monthlyOutput.textContent = `Monthly Hourly Rate: $${hourlyRate.toFixed(2)}`;
+});
+
+yearlySalaryInput.addEventListener("input", function () {
+  const yearlySalary = parseFloat(yearlySalaryInput.value);
+  const hourlyRate = calculateHourlyRate(yearlySalary, 1920); // Assuming 12 months in a year
+  yearlyOutput.textContent = `Yearly Hourly Rate: $${hourlyRate.toFixed(2)}`;
+});
+
+quarterlySalaryInput.addEventListener("input", function () {
+  const quarterlySalary = parseFloat(quarterlySalaryInput.value);
+  const hourlyRate = calculateHourlyRate(quarterlySalary, 480); // Assuming 3 months in a quarter
+  quarterlyOutput.textContent = `Quarterly Hourly Rate: $${hourlyRate.toFixed(2)}`;
+});
+
+biMonthlySalaryInput.addEventListener("input", function () {
+  const biMonthlySalary = parseFloat(biMonthlySalaryInput.value);
+  const hourlyRate = calculateHourlyRate(biMonthlySalary, 320); // Assuming 2 months in a bi-monthly period
+  biMonthlyOutput.textContent = `Bi-Monthly Hourly Rate: $${hourlyRate.toFixed(2)}`;
 });
